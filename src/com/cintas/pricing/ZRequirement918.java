d28f50b58c7c2950940af09bc536787a84b01340
Michael Josephson <michael@mertisconsulting.com>
2016-05-10 16:48:56 -0400

    Fix ancillary materials

--- src/com/cintas/pricing/ZRequirement918.java
+++ src/com/cintas/pricing/ZRequirement918.java
@@ -10,6 +10,25 @@ public class ZRequirement918 extends RequirementAdapter {
   public boolean checkRequirement(IConditionFindingManagerUserExit item,
       IStep step, IAccess access) {
     
-    return CintasConstants.IsProductAncillary(item);
+    String product = item.getAttributeValue(CintasConstants.Attributes.PRODUCT);
+    String conditionType = (step.getConditionType().getName() != null ? step.getConditionType().getName() : CintasConstants.INITIAL);
+    
+    if (conditionType.equals(CintasConstants.Conditions.SERVICE_CHARGE) || conditionType.equals(CintasConstants.Conditions.SubTotals.SERVICE_CHARGE))
+      return product.equals(item.getAttributeValue(CintasConstants.AncillaryMaterials.SERVICE));
+    
+    if (conditionType.equals(CintasConstants.Conditions.Rules.STOP_MIN) ||
+        conditionType.equals(CintasConstants.Conditions.STOP_MIN) ||
+        conditionType.equals(CintasConstants.Conditions.ADJ_MINIMUM) ||
+        conditionType.equals(CintasConstants.Conditions.SubTotals.MINIMUM_CHARGE))
+      return product.equals(item.getAttributeValue(CintasConstants.AncillaryMaterials.MINIMUM));
+    
+    String usage = item.getAttributeValue(CintasConstants.Attributes.USAGE);
+    
+    if (usage.equals(CintasConstants.Usage.BUYBACK))
+      return false;
+    if (usage.equals(CintasConstants.Usage.CHARGES))
+      return false;
+
+   return CintasConstants.IsProductAncillary(item);
   }
 }
--- src/com/cintas/pricing/ZRequirement926.java
+++ src/com/cintas/pricing/ZRequirement926.java
@@ -10,16 +10,12 @@ public class ZRequirement926 extends RequirementAdapter {
   public boolean checkRequirement(IConditionFindingManagerUserExit item,
       IStep step, IAccess access) {
 
-    if (CintasConstants.IsProductAncillary(item))
+    if (!item.getAttributeValue(CintasConstants.Attributes.PRODUCT).equals(item.getAttributeValue(CintasConstants.AncillaryMaterials.SERVICE)))
       return false;
 
     String usage = item.getAttributeValue(CintasConstants.Attributes.USAGE);
     
     if (usage.equals(CintasConstants.Usage.BUYBACK))
-      return false;
-    if (usage.equals(CintasConstants.Usage.LOST))
-      return false;
-    if (usage.equals(CintasConstants.Usage.DESTROY))
       return false;
     if (usage.equals(CintasConstants.Usage.CHARGES))
       return false;
